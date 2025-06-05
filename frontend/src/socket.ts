5e15f6cd56abc573e36ce9b56641cd1cc8a8817b
R0manOza <161681342+R0manOza@users.noreply.github.com>
2025-06-05 23:02:13 +0400

    Update socket.ts

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 import type { ServerToClientEvents, ClientToServerEvents } from './game';
 import { Socket } from 'socket.io-client';
 
-const SERVER_URL = process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3001';
+const SERVER_URL = 'http://172.31.34.176/:3002';
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SERVER_URL, {
   autoConnect: false 
